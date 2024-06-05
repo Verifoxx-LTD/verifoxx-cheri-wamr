a8f3c11a375d554fc1f9eace3737becc413de968
Peter Dunne <peter@dunnesoftltd.onmicrosoft.com>
2024-06-05 01:58:31 +0100

    REG1-126: Support extended performance profiling, calculate native func time and module setup time

--- core/iwasm/interpreter/wasm_interp_classic.c
+++ core/iwasm/interpreter/wasm_interp_classic.c
@@ -925,6 +925,12 @@ wasm_interp_call_func_native(WASMModuleInstance *module_inst,
         return;
     }
 
+#if WASM_ENABLE_CHERI_PERF_PROFILING != 0
+    /* Measure time spent in native function */
+    uint64 native_func_time_start = os_time_get_boot_microsecond();
+#endif
+
+
     if (func_import->call_conv_wasm_c_api) {
         ret = wasm_runtime_invoke_c_api_native(
             (WASMModuleInstanceCommon *)module_inst, native_func_pointer,
@@ -947,6 +953,12 @@ wasm_interp_call_func_native(WASMModuleInstance *module_inst,
             func_import->signature, func_import->attachment, frame->lp,
             cur_func->param_cell_num, argv_ret);
     }
+
+#if WASM_ENABLE_CHERI_PERF_PROFILING != 0
+    /* Measure time spent in native function */
+    cur_func->total_native_exec_time +=
+        os_time_get_boot_microsecond() - native_func_time_start;
+#endif
 
     if (!ret)
         return;
